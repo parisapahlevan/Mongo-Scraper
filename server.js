e6a07e47bae3a6ff28ffd73cb2c2bc8a4747f114
parisapahlevan <paris.pahlevan@gmail.com>
2019-05-14 11:43:46 -0400

    the server is ready now

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var PORT = process.env.PORT || 3000;
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var router = express.Router();
+app.use(express.static(__dirname + "/public"));
+ 
+
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(db, function(error){
+    if (error){
+        console.log(error);
+    } 
+    else {
+        console.log("mongoose connection is successfull");
+    }
+})
+
+app.listen(PORT, function(){
+    console.log("Listenig on port:" + PORT);
+});
