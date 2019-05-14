2b33e880389bb2e9385dbf6763405f156620371c
parisapahlevan <paris.pahlevan@gmail.com>
2019-05-14 19:17:57 -0400

    complete the route.js to render my  my hom and saved handlebars template

--- config/routes.js
+++ config/routes.js
@@ -1,1 +1,8 @@
- 
+ module.exports = function(router){
+     router.get("/", function(reg, res){
+         res.render("home");
+     });
+     router.get("/saved", function(req, res){
+         res.render("saved");
+     });
+ }
